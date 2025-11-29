1c8f6036b734ec7a239dff1cf69c5044e77959f9
Ryan Jing <ryanjing23@gmail.com>
2025-11-29 15:16:02 -0500

    Remove build folders from tracking

--- cube/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ryanjing/Local/Projects-Local/Embedded/C-Practice/cube
-# Build directory: /Users/ryanjing/Local/Projects-Local/Embedded/C-Practice/cube/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
